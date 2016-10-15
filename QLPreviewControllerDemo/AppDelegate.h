4ce60f18618f9a4c2756f93873ea27105c6b739e
happycast <lt@hpplay.cn>
2016-10-15 18:22:29 +0800

    Initial Commit

---
+++ QLPreviewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLPreviewControllerDemo
+//
+//  Created by Mossion on 16/10/15.
+//  Copyright © 2016年 Mossion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QLPreviewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLPreviewControllerDemo
+//
+//  Created by Mossion on 16/10/15.
+//  Copyright © 2016年 Mossion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
